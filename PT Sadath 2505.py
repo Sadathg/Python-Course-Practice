a878ca695afb7b08601f40c366f30189b18ecbd2
Sadathg <84565307+Sadathg@users.noreply.github.com>
2021-05-26 19:50:27 +0530

    course training python 3 days
    files of 2 and half days uploaded.
    
    Kindly share the complete file of today from other user.

---
+++ PT Sadath 2505.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+students = ['rohit','mounika','mehreen','kiran', 'abdul']
+
+
+# In[2]:
+
+
+print(studentsnts)
+
+
+# In[3]:
+
+
+type(students)
+
+
+# In[7]:
+
+
+students.append('divya')
+
+
+# In[25]:
+
+
+students.append('Sadath')
+
+
+# In[ ]:
+
+
+
+
+
+# In[26]:
+
+
+print(students)
+
+
+# In[18]:
+
+
+print(students[7])
+
+
+# In[22]:
+
+
+print(students)
+
+
+# In[23]:
+
+
+students = ['rohit', 'mounika', 'mehreen', 'kiran', 'abdul', 'divya', 'Sadath']
+
+
+# In[24]:
+
+
+print(students)
+
+
+# In[27]:
+
+
+students[0] = ['naman']
+
+
+# In[28]:
+
+
+print(students)
+
+
+# In[30]:
+
+
+students[0] = 'naman'
+
+
+# In[31]:
+
+
+print(students)
+
+
+# In[32]:
+
+
+del students[3]
+
+
+# In[33]:
+
+
+print(students)
+
+
+# In[35]:
+
+
+x = students.pop()     
+
+
+# In[36]:
+
+
+print(students)
+
+
+# In[38]:
+
+
+print(x)
+
+
+# In[ ]:
+
+
+
+
